3a79898023123f74da3a413b7ef00b6657f8b230
MeifiArifiani <48308127+MeifiArifiani@users.noreply.github.com>
2019-03-07 13:03:11 +0700

    Add files via upload

---
+++ MeifiAR.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Meifi;
+
+/**
+ *
+ * @author Meifi
+ */
+public class MeifiAR {
+    public static void main(String[] args) {
+        System.out.println("Nama : Meifi Arifiani");
+        System.out.println("NIM : 17090061 ");
+        System.out.println("Kelas : 4A ");
+    }
+}
